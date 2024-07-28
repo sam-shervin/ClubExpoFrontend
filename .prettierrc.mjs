0ad7390098bdddca92c5d82cc6ffc9403967c62e
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-29 03:18:41 +0530

    first commit

---
+++ .prettierrc.mjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+      {
+          files: "*.astro",
+          options: {
+              parser: "astro",
+          },
+      },
+  ],
+  tabWidth: 4,
+  include: ["src/**/*.svg"],
+};
